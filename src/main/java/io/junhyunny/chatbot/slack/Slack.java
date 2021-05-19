f59df98eee357c9d52db8c2d443b998ef84bdfbc
Junhyunny <kang3966@naver.com>
2021-05-19 23:32:27 +0900

    Test: Slack getMemberInChannel 테스트 추가

--- src/main/java/io/junhyunny/chatbot/slack/Slack.java
+++ src/main/java/io/junhyunny/chatbot/slack/Slack.java
@@ -1,78 +1,102 @@
 package io.junhyunny.chatbot.slack;
 
+import io.junhyunny.entity.GeneuinMember;
+import io.junhyunny.utils.JsonUtils;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.extern.log4j.Log4j2;
-
 @Log4j2
 public class Slack {
 
-	private final String slackToken;
 
-	private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-	private final String[] messages = new String[] { "commit 할 시간이야.", "commit은 하고 노는거야?", "commit 안하면 밥도 먹지마!", "어린애처럼 굴래? 열심히 한다며?" };
+    private final HttpHeaders headers;
 
-	public Slack(String slackToken) {
-		this.slackToken = slackToken;
-		this.restTemplate = new RestTemplate();
-	}
+    private final String[] messages = new String[]{"commit 할 시간이에요.", "오늘 commit 하고 놀고 있는거에요?", "commit 안하면 밥도 먹지마세요.", "미래를 위해 공부해야죠. 어린애처럼 굴래요?"};
 
-	private HttpHeaders getCommonHeader() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Content-Type", "application/json; charset=utf-8");
-		headers.set("Authorization", "Bearer " + slackToken);
-		return headers;
-	}
+    public Slack(String slackToken) {
+        restTemplate = new RestTemplate();
+        headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json; charset=utf-8");
+        headers.set("Authorization", "Bearer " + slackToken);
+    }
 
-	@SuppressWarnings({ "unchecked" })
-	private String getChannelId(String channelName) {
+    @SuppressWarnings({"unchecked"})
+    public String getChannelId(String channelName) {
 
-		String result = null;
+        String result = null;
 
-		HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(getCommonHeader());
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(headers);
 
-		Map<String, List<Map<String, Object>>> response = restTemplate.exchange("https://slack.com/api/conversations.list", HttpMethod.GET, entity, Map.class).getBody();
-		if (response == null) {
-			return result;
-		}
+        Map<String, List<Map<String, Object>>> response = restTemplate.exchange("https://slack.com/api/conversations.list", HttpMethod.GET, entity, Map.class).getBody();
+        if (response == null) {
+            return null;
+        }
 
-		List<Map<String, Object>> channels = response.get("channels");
-		for (Map<String, Object> channel : channels) {
-			if (channelName.equals(channel.get("name"))) {
-				result = channel.get("id") + "";
-				break;
-			}
-		}
+        List<Map<String, Object>> channels = response.get("channels");
+        for (Map<String, Object> channel : channels) {
+            if (channelName.equals(channel.get("name"))) {
+                result = channel.get("id") + "";
+                break;
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public void sendPushMessage(String channelName) {
+    public Map<String, GeneuinMember> getMembersInChannel(String channelId) {
 
-		String channelId = getChannelId(channelName);
-		if(channelId == null) {
-			log.error("channel id를 구하지 못하였습니다.");
-			return;
-		}
+        Map<String, GeneuinMember> result = new HashMap<>();
 
-		String message = messages[new Random().nextInt(999) % 4];
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(headers);
 
-		Map<String, Object> body = new HashMap<>();
-		body.put("text", message);
-		body.put("reply_broadcast", true);
-		body.put("channel", channelId);
+        StringBuffer url = new StringBuffer("https://slack.com/api/conversations.members");
+        url.append("?channel=").append(channelId);
 
-		HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(body, getCommonHeader());
+        Map<String, List<Map<String, Object>>> response = restTemplate.exchange(url.toString(), HttpMethod.GET, entity, Map.class).getBody();
+        if (response == null) {
+            return null;
+        }
 
-		log.info("api call response message: " + restTemplate.exchange("https://slack.com/api/chat.postMessage", HttpMethod.POST, entity, Map.class).getBody());
-	}
+        url.delete(0, url.length());
+        url.append("https://slack.com/api/users.info");
+        url.append("?user=");
+
+        List<String> memberKeys = (ArrayList) response.get("members");
+        for (String memberKey : memberKeys) {
+            Map<String, Object> mapMember = restTemplate.exchange(url + memberKey, HttpMethod.GET, entity, Map.class).getBody();
+            result.put(memberKey, JsonUtils.fromJson(JsonUtils.toJson(mapMember.get("user")), GeneuinMember.class));
+        }
+
+        return result;
+    }
+
+    public void sendPushMessage(String channelName) {
+
+        String channelId = getChannelId(channelName);
+        if (channelId == null) {
+            log.error("channel id를 구하지 못하였습니다.");
+            return;
+        }
+
+        String message = messages[new Random().nextInt(999) % 4];
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("text", message);
+        body.put("reply_broadcast", true);
+        body.put("channel", channelId);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(body, headers);
+
+        log.info("api call response message: " + restTemplate.exchange("https://slack.com/api/chat.postMessage", HttpMethod.POST, entity, Map.class).getBody());
+    }
 }
---
+++ src/test/java/io/junhyunny/slack/SlackTest.java
@@ -0,0 +1,16 @@
+package io.junhyunny.slack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import io.junhyunny.chatbot.slack.Slack;
+import org.junit.jupiter.api.Test;
+
+public class SlackTest {
+
+    private String slackToken = "your slack token";
+
+    @Test
+    public void getMembersInChannel_normalChannelName_memberListSizeNotZero() {
+        Slack slack = new Slack(slackToken);
+        assertThat(slack.getMembersInChannel(slack.getChannelId("one-day-one-commit"))).isNotEmpty();
+    }
+}
